64fa5d2098b051531d7169888172f8eed0138e8b
Bassem <bassem@coradine.com>
2020-11-26 23:33:40 +0100

    Updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Design+Code`,
+    description: `Don’t skip design. Learn design and code by building real apps with React and Swift. Complete courses about the best tools.`,
+    author: `@nextglabs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
