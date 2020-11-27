aa7f155f94d1e51a7ed5819841bb3571ef68aea8
Bassem <bassem@coradine.com>
2020-11-27 03:10:41 +0100

    deps: Added styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
         path: `${__dirname}/static/images`,
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
