80361d76ec54da17dcba1719397ef9bc94530f11
Bassem <bassem@coradine.com>
2020-11-27 15:23:21 +0100

    Updated Layout & Hero

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { GlobalStyle } from "../styles/GlobalStyle"
+import Header from "./Header"
 import "./layout.css"
 
 function Layout({ children }) {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <main>{children}</main>
     </>
   )
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,29 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import MockupAnimation from "../animations/MockupAnimation";
+import WaveBackground from "../backgrounds/WaveBackground";
+import PurchaseButton from "../button/PurchaseButton";
 import { themes } from "../styles/Color";
 import { H1, MediumText } from "../styles/Text";
 
 export default function Hero() {
     return (
         <Wrapper>
+            <WaveBackground />
             <ContentWrapper>
                 <TextWrapper>
                     <Title>Design<br />and Code React Apps</Title>
                     <Description>Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.</Description>
+                    <PurchaseButton title="Get Pro Access" subtitle="$19 per month" />
                 </TextWrapper>
+                <MockupAnimation />
             </ContentWrapper>
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
-    background: linear-gradient(180deg, #4316DB 0%, #9076E7 100%);
+    overflow: hidden;
 `;
 
 const ContentWrapper = styled.div`
     max-width: 1234px;
     margin: 0 auto;
     padding: 200px 30px;
+    display: grid;
+    grid-template-columns: 360px auto;
 `;
 
 const TextWrapper = styled.div`
