4d8dcdd0870d77c59bc368a6b77bb77c81cf86fe
Bassem <bassem@coradine.com>
2020-11-27 23:06:40 +0100

    feat: Added gatsby plugin manifest + favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,20 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Design+Code`,
+        short_name: `Design+Code`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#3913B8`,
+        display: `standalone`,
+        icon: `${__dirname}/static/images/logos/logo.svg`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
